894aa238a868174a9f2a3b2269ecf1bcbf3cfcee
pautina <pautina.dmitry@gmail.com>
2019-02-19 02:30:03 +0400

    HW0

--- src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
@@ -3,6 +3,15 @@ package ru.javawebinar.topjava.util;
 import java.time.LocalTime;
 
 public class TimeUtil {
+
+    /**
+     * Проверка того, если время находится в указанных промежутках включительно
+     *
+     * @param lt
+     * @param startTime
+     * @param endTime
+     * @return boolean
+     */
     public static boolean isBetween(LocalTime lt, LocalTime startTime, LocalTime endTime) {
         return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) <= 0;
     }
